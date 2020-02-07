a5e8a12824b6bca64fe0f3478a574acdc04f03a9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-07 15:05:38 +0000

    Deploy VokkeRr/Tarif to github.com/VokkeRr/Tarif.git:gh-pages

---
+++ precache-manifest.f2ceb03c7d896e6ee588c1bff504f1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b449ebaff9e9f02d198f999554ea55",
+    "url": "/Tarif/index.html"
+  },
+  {
+    "revision": "3d601d96b1b43e9c9ea8",
+    "url": "/Tarif/static/css/main.6203d8d9.chunk.css"
+  },
+  {
+    "revision": "e5139657e01045480748",
+    "url": "/Tarif/static/js/2.0bfd1891.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Tarif/static/js/2.0bfd1891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d601d96b1b43e9c9ea8",
+    "url": "/Tarif/static/js/main.0ea7c72f.chunk.js"
+  },
+  {
+    "revision": "2867b6513f0b9c5d9a7f",
+    "url": "/Tarif/static/js/runtime-main.33a85a94.js"
+  }
+]);
